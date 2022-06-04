c7510111b3819cd3b5c3b70380cc132722700b17
victorsoft32 <106864482+victorsoft32@users.noreply.github.com>
2022-06-04 09:59:00 +0100

    Add files via upload
    memory

---
+++ memory.js
@@ -0,0 +1,57 @@
+
+let memoryAddress = [
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 
+  1, 0, 1, 0, 1, 0, 1, 0, 1, 
+  1, 1, 0, 0, 1, 1, 1, 0, 1, 
+  1, 0, 1, 0, 1, 0, 1, 0, 1
+  ];//ram bytes
+const hardDiskSize = 20000;//harddisk size 20GB
+
+class buildInternalCpu{
+  constructor(){
+    this.memoryAddr = memoryAddress;
+    this.harddiskSize = hardDiskSize;
+    this.segementedAppaddress = 12;
+  }
+  
+  
+    checkRam(){
+      if(this.memoryAddr=!this.sys){
+        this.sys = 0;
+        while(this.sys<=100){
+          try{
+       return Math.random(0, this.memoryAddr+2*2/10);
+        //do not excute thid
+        this.sys++;
+        
+          }
+          catch{
+            this.memoryAddr;
+          }
+          
+          
+        }
+      }else{
+        console.log("RAM NOT BUFFERED");
+      }
+    }
+    
+    
+    checkHardDisk(){
+      this.hardd = this.harddiskSize;
+      if(this.hardd>0){
+        return this.hardd;
+      }
+      else{
+        return 0;
+      }
+    }
+    
+  
+  
+}
+
+
+let addr = new buildInternalCpu();
+console.log("Memory Installed: "+addr.checkRam()/2000);
+console.log("Hard disk Installed: " +addr.checkHardDisk()+"MB");
